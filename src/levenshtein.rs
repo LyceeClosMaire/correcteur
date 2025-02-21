8f1c0eaddd5603e00025bd160862dda822269b8c
Chaddaï Fouché <chaddai.fouche@gmail.com>
2025-02-21 13:55:57 +0100

    Clean up non-necessary code

--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -1,6 +1,3 @@
-extern crate regex;
-extern crate ndarray;
-
 use std::cmp::min;
 use regex::Regex;
 use ndarray::prelude::*;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-extern crate encoding_rs;
-extern crate encoding_rs_io;
-
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
 use encoding_rs::WINDOWS_1252;
@@ -9,13 +6,17 @@ use encoding_rs_io::DecodeReaderBytesBuilder;
 mod levenshtein;
 
 fn main() -> std::io::Result<()> {
-    let decoder = DecodeReaderBytesBuilder::new().encoding(Some(WINDOWS_1252)).build(File::open("liste_francais.txt").unwrap());
+    let decoder = DecodeReaderBytesBuilder::new()
+        .encoding(Some(WINDOWS_1252))
+        .build(File::open("liste_francais.txt")
+        .unwrap());
     let reader = BufReader::new(decoder);
-    let dico : Vec<String> = reader.lines().into_iter().map(|m| m.unwrap()).collect();
+    let dico : Vec<String> = reader.lines().map(|m| m.unwrap()).collect();
     let mut texte = String::new();
 
     println!("Entrez votre texte à corriger à la ligne suivante :");
     io::stdin().read_line(&mut texte).unwrap();
+    
     levenshtein::corrige(dico, texte);
     
     return Ok(());
